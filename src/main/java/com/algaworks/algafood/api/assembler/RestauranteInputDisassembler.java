288be828773956495119474f3144aef2b06b27cc
Tony Costa <tonyblack10@gmail.com>
2023-02-19 04:27:47 -0300

    mapeando para uma instância destino (e não um tipo) com ModelMapper

--- src/main/java/com/algaworks/algafood/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/RestauranteInputDisassembler.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.assembler;
 
 import com.algaworks.algafood.api.model.input.RestauranteInput;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,14 @@ public class RestauranteInputDisassembler {
 
     public Restaurante toDomainObject(RestauranteInput restauranteInput) {
        return modelMapper.map(restauranteInput, Restaurante.class);
+    }
+
+    public void copyToDomainObject(RestauranteInput restauranteInput, Restaurante restaurante) {
+        // Para evitar org.hibernate.HibernateException: identifier of an instance of
+        // com.algaworks.algafood.domain.model.Cozinha was altered from 1 to 2
+        restaurante.setCozinha(new Cozinha());
+
+        modelMapper.map(restauranteInput, restaurante);
     }
 
 }
