bd8b7964ccaa2fa4d5ae4c72f2b274ae9a3867ed
Tony Costa <tonyblack10@gmail.com>
2023-02-20 00:33:07 -0300

    desafio: Implementando os endpoints de abertura e fechamento de restaurantes

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -50,6 +50,8 @@ public class Restaurante {
 
 	private Boolean ativo = Boolean.TRUE;
 
+	private Boolean aberto = Boolean.FALSE;
+
 	@CreationTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
 	private OffsetDateTime dataCadastro;
@@ -73,6 +75,14 @@ public class Restaurante {
 
 	public void inativar() {
 		setAtivo(false);
+	}
+
+	public void abrir() {
+		setAberto(true);
+	}
+
+	public void fechar() {
+		setAberto(false);
 	}
 
 	public boolean removerFormaPagamento(FormaPagamento formaPagamento) {
