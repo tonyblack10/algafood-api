dc169d9a0dc6609ea40b74b834920fffc2dd50eb
Tony Costa <tonyblack10@gmail.com>
2023-05-01 14:28:18 -0300

    implementando um cliente com o fluxo Authorization Code

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,7 +21,7 @@ public class ResourceServerConfig {
             .cors().and()
             .oauth2ResourceServer().jwt();
 
-        return http.build();
+        return http.formLogin(Customizer.withDefaults()).build();
     }
 
 }
